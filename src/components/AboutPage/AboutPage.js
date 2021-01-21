e376c1d79ce83ba66471c94b6b0e77f6624404b4
James Posey <jamesrposeyjr@gmail.com>
2021-01-21 07:24:15 -0600

    added basic info to about page

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -3,7 +3,18 @@ import React from 'react';
 const AboutPage = () => (
   <div className='container'>
     <div>
-      <p>This about page is for anyone to read!</p>
+      <p>
+        A gamified classroom allows teachers to easily and quickly reward
+        students for participating in the classroom and keep track of all
+        students and activity levels throughout one or many classrooms. The
+        gamified classroom is aimed to be quick, simple, and easy with limited
+        intrusiveness to allow for quick approval by parents and school boards.
+      </p>
+      <p>
+        This application was made in a two-week sprint for a middle school
+        teacher in VA. I used Javasript, React, Redux, Redux-Sagas, Material-UI
+        Node, Express, PostgreSQL, Websockets, Nodemailer, and Passport.
+      </p>
     </div>
   </div>
 );
