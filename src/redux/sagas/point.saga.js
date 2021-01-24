e2fb4c15777b25c0d811978f052e785e932bae05
James Posey <jamesrposeyjr@gmail.com>
2021-01-24 14:06:12 -0600

    documented point saga

--- src/redux/sagas/point.saga.js
+++ src/redux/sagas/point.saga.js
@@ -2,13 +2,14 @@ import { put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 
 /**
- * Gets the current selected student's id
+ * Send an id to the server to update the points in the DB for the selected student
  * @param {Number} action Action payload that holds the id
  * */
+
 function* getPointStudent(action) {
   const id = action.payload;
   try {
-    const results = yield axios.get(`/api/student/point/${id}`);
+    const results = yield axios.put(`/api/student/point/${id}`);
     yield put({ type: 'SET_POINT_STUDENT', payload: results.data });
   } catch (error) {
     console.log('Error with getting point Student data:', error);
