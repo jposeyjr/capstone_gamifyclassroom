c661cf47878c71644420b721dee6d7cae6d7736c
James Posey <jamesrposeyjr@gmail.com>
2021-01-25 20:23:51 -0600

    fixed docker files

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -17,12 +17,28 @@ if (process.env.DATABASE_URL) {
     idleTimeoutMillis: 30000,
   };
 } else {
+  const host = process.env.POSTGRES_HOST
+    ? process.env.POSTGRES_HOST
+    : 'localhost'; // "localhost" is Docker's default
+  const user = process.env.POSTGRES_USER
+    ? process.env.POSTGRES_USER
+    : 'postgres'; // "postgres" is the default username for Docker
+  const password = process.env.POSTGRES_PASSWORD
+    ? process.env.POSTGRES_PASSWORD
+    : 'postgres'; // "postgres" is the default password for Docker
+  const port = process.env.PORT_DB ? process.env.PORT_DB : 5432;
+  const database = process.env.POSTGRES_DB
+    ? process.env.POSTGRES_DB
+    : 'capstone';
+
   config = {
-    host: 'localhost',
-    port: 5432,
-    database: 'capstone',
-    max: 10,
-    idleTimeoutMillis: 30000,
+    host, // Server hosting the postgres database
+    user,
+    password,
+    port,
+    database, // name of application database
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
 }
 
