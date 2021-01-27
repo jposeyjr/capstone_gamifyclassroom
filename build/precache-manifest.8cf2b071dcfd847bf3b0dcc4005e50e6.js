e844a431822993c7d9ddcfe4d1770bb428adf896
James Posey <jamesrposeyjr@gmail.com>
2021-01-27 14:50:30 -0600

    build for client

---
+++ build/precache-manifest.8cf2b071dcfd847bf3b0dcc4005e50e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c98ae83e31fbf97f54839083a2467e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cb3559dafb473caefa7",
+    "url": "/static/css/main.d1fd01d5.chunk.css"
+  },
+  {
+    "revision": "4660271f004c829bdd03",
+    "url": "/static/js/2.35e3f94a.chunk.js"
+  },
+  {
+    "revision": "8041207c6ef8ff1fb65254e32bc18cb7",
+    "url": "/static/js/2.35e3f94a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cb3559dafb473caefa7",
+    "url": "/static/js/main.32488396.chunk.js"
+  },
+  {
+    "revision": "cfabed5267c41a13b59e",
+    "url": "/static/js/runtime-main.ddf8fe10.js"
+  }
+]);
